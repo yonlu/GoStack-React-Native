2e1c5aad6114bbb868599076423711bd9617abc4
yonlu <lss.emailbox@gmail.com>
2019-07-16 16:20:04 -0400

    feature: added async storage

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
+
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
@@ -30,6 +31,7 @@ export const SubmitButton = styled(RectButton)`
   border-radius: 4px;
   margin-left: 10px;
   padding: 0 12px;
+  opacity: ${props => (props.loading ? 0.7 : 1)};
 `;
 
 export const List = styled.FlatList.attrs({
